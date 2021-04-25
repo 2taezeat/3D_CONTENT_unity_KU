2c02c3647c47f6e1cd1b4696ed6200c147049fc3
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-04-25 15:19:37 +0900

    210425_3d_pratice

---
+++ mid_project/practice_3d/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{	
+	public int jumpPower = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.Space))
+		// {
+			// GetComponent<Rigidbody>().velocity = new Vector3(0,5,0); // space를 누르면 y축이 5만큼 올라감.
+		// }
+		
+		if(Input.GetButtonDown("Fire1"))
+	 	{
+			GetComponent<Rigidbody>().velocity = new Vector3(0,jumpPower,0); // space를 누르면 y축이 5만큼 올라감.
+		}
+		
+
+    }
+}
+
+
